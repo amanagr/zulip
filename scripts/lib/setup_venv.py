e59d0e0b6d06aaab2024d3985e2c36a577b68c08
Aman Agrawal <f2016561@pilani.bits-pilani.ac.in>
2020-04-17 20:50:27 +0530

    setup: Remove python2 support and python2 packages.

--- scripts/lib/setup_venv.py
+++ scripts/lib/setup_venv.py
@@ -24,7 +24,6 @@ VENV_DEPENDENCIES = [
     "libmemcached-dev",
     "python3-dev",          # Needed to install typed-ast dependency of mypy
     "python3-pip",
-    "python-pip",
     "virtualenv",
     "python3-six",
     "python-six",
@@ -44,10 +43,6 @@ VENV_DEPENDENCIES = [
     "jq",                   # Used by scripts/lib/install-node to check yarn version
 ]
 
-# python-dev is depreciated in Focal but can be used as python2-dev.
-# So it is removed from VENV_DEPENDENCIES and added here.
-PYTHON_DEV_DEPENDENCY = "python{}-dev"
-
 COMMON_YUM_VENV_DEPENDENCIES = [
     "libffi-devel",
     "freetype-devel",
@@ -56,8 +51,6 @@ COMMON_YUM_VENV_DEPENDENCIES = [
     "openldap-devel",
     "libmemcached-devel",
     "python-devel",
-    "python2-pip",
-    "python-six",
     # Needed by python-xmlsec:
     "gcc"
     "python3-devel",
@@ -65,7 +58,6 @@ COMMON_YUM_VENV_DEPENDENCIES = [
     "xmlsec1-devel",
     "xmlsec1-openssl-devel",
     "libtool-ltdl-devel",
-
     "libxslt-devel",
     "postgresql-libs",  # libpq-dev on apt
     "openssl-devel",
@@ -105,9 +97,9 @@ YUM_THUMBOR_VENV_DEPENDENCIES = [
 def get_venv_dependencies(vendor, os_version):
     # type: (str, str) -> List[str]
     if vendor == 'ubuntu' and os_version == '20.04':
-        return VENV_DEPENDENCIES + [PYTHON_DEV_DEPENDENCY.format("2"), ]
+        return VENV_DEPENDENCIES
     elif "debian" in os_families():
-        return VENV_DEPENDENCIES + [PYTHON_DEV_DEPENDENCY.format(""), ]
+        return VENV_DEPENDENCIES
     elif "rhel" in os_families():
         return REDHAT_VENV_DEPENDENCIES
     elif "fedora" in os_families():
@@ -115,9 +107,9 @@ def get_venv_dependencies(vendor, os_version):
     else:
         raise AssertionError("Invalid vendor")
 
-def install_venv_deps(pip, requirements_file, python2):
-    # type: (str, str, bool) -> None
-    pip_requirements = os.path.join(ZULIP_PATH, "requirements", "pip2.txt" if python2 else "pip.txt")
+def install_venv_deps(pip, requirements_file):
+    # type: (str, str) -> None
+    pip_requirements = os.path.join(ZULIP_PATH, "requirements", "pip.txt")
     run([pip, "install", "--force-reinstall", "--require-hashes", "--requirement", pip_requirements])
     run([pip, "install", "--no-deps", "--require-hashes", "--requirement", requirements_file])
 
@@ -290,8 +282,8 @@ def do_patch_activate_script(venv_path):
     with open(script_path, 'w') as f:
         f.write("".join(lines))
 
-def setup_virtualenv(target_venv_path, requirements_file, python2=False, patch_activate_script=False):
-    # type: (Optional[str], str, bool, bool) -> str
+def setup_virtualenv(target_venv_path, requirements_file, patch_activate_script=False):
+    # type: (Optional[str], str, bool) -> str
 
     # Check if a cached version already exists
     path = os.path.join(ZULIP_PATH, 'scripts', 'lib', 'hash_reqs.py')
@@ -303,7 +295,7 @@ def setup_virtualenv(target_venv_path, requirements_file, python2=False, patch_a
         cached_venv_path = os.path.join(VENV_CACHE_PATH, sha1sum, os.path.basename(target_venv_path))
     success_stamp = os.path.join(cached_venv_path, "success-stamp")
     if not os.path.exists(success_stamp):
-        do_setup_virtualenv(cached_venv_path, requirements_file, python2)
+        do_setup_virtualenv(cached_venv_path, requirements_file)
         with open(success_stamp, 'w') as f:
             f.close()
 
@@ -321,8 +313,8 @@ def add_cert_to_pipconf():
     os.makedirs(confdir, exist_ok=True)
     run(["crudini", "--set", conffile, "global", "cert", os.environ["CUSTOM_CA_CERTIFICATES"]])
 
-def do_setup_virtualenv(venv_path, requirements_file, python2):
-    # type: (str, str, bool) -> None
+def do_setup_virtualenv(venv_path, requirements_file):
+    # type: (str, str) -> None
 
     # Setup Python virtualenv
     new_packages = set(get_package_names(requirements_file))
@@ -331,7 +323,7 @@ def do_setup_virtualenv(venv_path, requirements_file, python2):
     if not try_to_copy_venv(venv_path, new_packages):
         # Create new virtualenv.
         run_as_root(["mkdir", "-p", venv_path])
-        run_as_root(["virtualenv", "-p", "python2.7" if python2 else "python3", venv_path])
+        run_as_root(["virtualenv", "-p", "python3", venv_path])
         run_as_root(["chown", "-R",
                      "{}:{}".format(os.getuid(), os.getgid()), venv_path])
         create_log_entry(get_logfile_name(venv_path), "", set(), new_packages)
@@ -346,10 +338,10 @@ def do_setup_virtualenv(venv_path, requirements_file, python2):
         add_cert_to_pipconf()
 
     try:
-        install_venv_deps(pip, requirements_file, python2)
+        install_venv_deps(pip, requirements_file)
     except subprocess.CalledProcessError:
         # Might be a failure due to network connection issues. Retrying...
         print(WARNING + "`pip install` failed; retrying..." + ENDC)
-        install_venv_deps(pip, requirements_file, python2)
+        install_venv_deps(pip, requirements_file)
 
     run_as_root(["chmod", "-R", "a+rX", venv_path])
