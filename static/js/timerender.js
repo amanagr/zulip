8b518fc6aed07785b8c00388a1f92295b86dfc24
Aman Agrawal <amanagr@zulip.com>
2020-10-02 15:16:25 +0530

    last_seen_status_from_date: Fix incorrect last seen value.
    We use day_old calculated based on day instead of hours to
    render last seen values. This fixes us incorrectly quoting
    anything 24 - 48 hours ago as Yesterday and
    incorrectly quoting `time` that are Yesterday
    but < 24 hours ago in 'x hours ago' format.

--- static/js/timerender.js
+++ static/js/timerender.js
@@ -79,22 +79,22 @@ exports.last_seen_status_from_date = function (last_active_date, current_date) {
     if (minutes < 60) {
         return i18n.t("__minutes__ minutes ago", {minutes});
     }
-
+    const {days_old, is_older_year} = calculate_days_old_from_time(last_active_date, current_date);
     const hours = Math.floor(minutes / 60);
-    if (hours === 1) {
-        return i18n.t("An hour ago");
-    }
-    if (hours < 24) {
+
+    if (days_old === 0) {
+        if (hours === 1) {
+            return i18n.t("An hour ago");
+        }
         return i18n.t("__hours__ hours ago", {hours});
     }
 
-    const days = Math.floor(hours / 24);
-    if (days === 1) {
+    if (days_old === 1) {
         return i18n.t("Yesterday");
     }
 
-    if (days < 90) {
-        return i18n.t("__days__ days ago", {days});
+    if (days_old < 90) {
+        return i18n.t("__days_old__ days ago", {days_old});
     } else if (
         days > 90 &&
         days < 365 &&
