14bfb33dc756b9d885ea9f007522ac346990c226
Aman Agrawal <amanagr@zulip.com>
2020-12-03 09:33:44 +0530

    pricing: Redirect to landing pricing view with realm info.
    We pass the plan realm is subscribed to in the URL params
    and redirect to landing page pricing.
    
    Remove trail period since it will be configured manually in
    landing page form now on.

--- zerver/views/portico.py
+++ zerver/views/portico.py
@@ -10,17 +10,15 @@ from zerver.context_processors import get_realm_from_request, latest_info_contex
 from zerver.decorator import add_google_analytics
 from zerver.models import Realm
 
-@add_google_analytics
 def pricing_view(request: HttpRequest) -> HttpResponse:
     realm = get_realm_from_request(request)
     realm_plan_type = 0
-    free_trial_days = settings.FREE_TRIAL_DAYS
     sponsorship_pending = False
 
     if realm is not None:
         realm_plan_type = realm.plan_type
         if realm.plan_type == Realm.SELF_HOSTED and settings.PRODUCTION:
-            return HttpResponseRedirect('https://zulip.com/plans')
+            return HttpResponseRedirect(settings.LANDING_PAGE_URL + "/pricing")
         if not request.user.is_authenticated:
             return redirect_to_login(next="pricing")
         if request.user.is_guest:
@@ -31,11 +29,7 @@ def pricing_view(request: HttpRequest) -> HttpResponse:
             if customer is not None:
                 sponsorship_pending = customer.sponsorship_pending
 
-    return TemplateResponse(
-        request,
-        "zerver/plans.html",
-        context={"realm_plan_type": realm_plan_type, 'free_trial_days': free_trial_days, 'sponsorship_pending': sponsorship_pending},
-    )
+    return HttpResponseRedirect(settings.LANDING_PAGE_URL + f"/pricing?realm_plan_type={realm_plan_type}&sponsorship_pending={sponsorship_pending}")
 
 @add_google_analytics
 def team_view(request: HttpRequest) -> HttpResponse:
