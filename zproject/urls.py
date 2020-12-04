06391754d3fadcd875101aa5f86adcd1f931ee1e
Aman Agrawal <amanagr@zulip.com>
2020-12-04 18:25:40 +0530

    urls: Redirect /for/working-groups... relevant landing page url.
    Remove unused files.

--- zproject/urls.py
+++ zproject/urls.py
@@ -734,8 +734,7 @@ i18n_urls = [
     path('for/open-source/', RedirectView.as_view(url=settings.LANDING_PAGE_URL + "/solutions/open-source")),
     path('for/research/', RedirectView.as_view(url=settings.LANDING_PAGE_URL + "/solutions/research")),
     path('for/companies/', RedirectView.as_view(url=settings.LANDING_PAGE_URL + "/solutions/companies")),
-    path('for/working-groups-and-communities/', landing_view,
-         {'template_name': 'zerver/for-working-groups-and-communities.html'}),
+    path('for/working-groups-and-communities/', RedirectView.as_view(url=settings.LANDING_PAGE_URL + "/solutions/working-groups-and-communities")),
     path('security/', landing_view, {'template_name': 'zerver/security.html'}),
     path('atlassian/', landing_view, {'template_name': 'zerver/atlassian.html'}),
 
